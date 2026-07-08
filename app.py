c685cbf2ed20538d0fb4f735cb4cb61b7d6e4ba3
heroikiosan-boop <hero.ikiosan@gmail.com>
2026-07-08 18:48:53 +0330

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 WEEX_API_KEY = 'weex_57af930315aa859c641c180987f8ff5d'
 WEEX_SECRET_KEY = '92f41c4b5fbe11fced7aa776dd305ae2e6600fc6e985d3df2ce94e8947293859'
 WEEX_PASSPHRASE = 'Mosi4219sadra'
-WEEX_URL = "https://weex.com"
+WEEX_URL = "https://weexapi.com"
 WEBHOOK_SECRET_PASSWORD = "MY_SECURE_PASSWORD_123"
 
 def get_weex_sign(params, secret_key):
